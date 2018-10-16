8307fe0c37dc44e4501400870c9b33080fc91490
zhoujun <zhoujun1@icerno.com>
2018-10-16 11:47:23 +0800

    CodingRoboter---》项目基本架构设计。

---
+++ src/main/java/com/fangshuo/lib4fangshuo/oauth/oauthTest.java
@@ -0,0 +1,17 @@
+package com.fangshuo.lib4fangshuo.oauth;
+
+/**
+ * 开源认证的测试类； Copyright: Copyright (c) 2018 Jun_Zhou
+ * 
+ * @ClassName: oauthTest.java
+ * @Description: 该类的功能描述
+ * 
+ * @version: v1.0.0
+ * @author: JunZhou
+ * @Email: 1769676159@qq.com
+ * @Site: CERNO
+ * @date: 2018年10月15日 下午8:30:17
+ */
+public class oauthTest {
+
+}
